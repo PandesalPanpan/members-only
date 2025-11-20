881ff4632f13398b475fa5bd9645b4ce07f0736e
PandesalPanpan <petermarticio@gmail.com>
2025-11-20 17:35:46 +0800

    Feat: Redirect to login on auth routes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ module.exports.isAuth = (req, res, next) => {
     if (req.isAuthenticated()) {
         next();
     } else {
-        res.status(401).json({ message: "You are not authorized to view this resource." });
+        res.status(401).redirect('/login');
     }
 }
 
