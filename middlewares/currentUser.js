0f87bad093f3ae8ff93353d443d7b9a25abf5eda
PandesalPanpan <petermarticio@gmail.com>
2025-11-20 15:14:18 +0800

    Feat: Conditionally add username if user is detected

--- middlewares/currentUser.js
+++ middlewares/currentUser.js
@@ -4,5 +4,9 @@ module.exports.currentUserMiddleware = (req, res, next) => {
     res.locals.isAuthenticated = !!req.user;
     res.locals.isMember = !!(req.user && req.user.is_member);
     res.locals.isAdmin = !!(req.user && req.user.is_admin);
+    if (req.user) {
+        res.locals.username = req.user.username;
+    }
+
     next();
 }
